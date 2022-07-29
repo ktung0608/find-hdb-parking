d9bb826772ec66f59a09b537adcd08f9566b51ec
ktung0608 <29835369+ktung0608@users.noreply.github.com>
2022-07-29 14:33:54 +0800

    Update credentials.py

--- captain_digibot/credentials.py
+++ captain_digibot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "5511952686:AAH4UsfUyxGtkImQKKJoImZFA8g1rYdYC9Q"
-bot_user_name = "HDBparking_bot"
-URL = "https://find-hdb-parking.herokuapp.com/"
+bot_token = "{your bot token}"
+bot_user_name = "{your bot name}"
+URL = "{your app url}"
